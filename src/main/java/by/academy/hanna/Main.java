4b3a907c067a6d8e4e4b63ba11c5daa5aa5ef549
hanna_klimasheuskaya <hanna.klimasheuskaya@gmail.com>
2022-11-24 18:51:30 +0300

    add task01

---
+++ src/main/java/by/academy/hanna/Main.java
@@ -0,0 +1,8 @@
+package by.academy.hanna;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User(1, "Anna", "32");
+        System.out.println(user);
+    }
+}
